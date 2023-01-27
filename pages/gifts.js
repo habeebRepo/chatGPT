0d158a94a3f7f16bed90ba24e768863e46250d61
habeebRepo <rahmanhabeeb08@gmail.com>
2023-01-27 18:41:11 +0530

    updated title

--- pages/gifts.js
+++ pages/gifts.js
@@ -47,7 +47,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
+        <title>ChatGPT</title>
         <link rel="icon" href="/dog.png" />
       </Head>
 
